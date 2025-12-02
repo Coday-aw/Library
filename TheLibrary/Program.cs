eb3ff744f8987b9c8e755bf097b6b3ef6f847d5c
coday-aw <coday-aw@hotmail.com>
2025-12-02 16:47:13 +0100

    first commit

---
+++ TheLibrary/Program.cs
@@ -0,0 +1,16 @@
+﻿using TheLibrary.Models;
+
+namespace TheLibrary;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using var db = new LibraryDbContext();
+        var allActiveLoans = db.ActiveLoans.ToList();
+        foreach (var loan in allActiveLoans)
+        {
+            Console.WriteLine(loan.LoanId);
+        }
+    }
+}
